c9bcf7ba7b8137789e65dfd17edae92b6755adfe
Siydul Islam <siydulislam333@gmail.com>
2022-05-07 22:46:51 +0600

    database connected

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = 5000;
@@ -6,6 +8,22 @@ const port = 5000;
 app.get('/', (req, res) => {
     res.send("Welcome to my server");
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vt6on.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri)
+
+const run = async () => {
+    try {
+        await client.connect();
+        console.log("DB connected");
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+run();
+
 
 app.listen(port, () => {
     console.log("server is running");
